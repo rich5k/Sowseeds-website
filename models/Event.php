30a426fe76c1f10506d95c930a81ff4e020f5fd5
rich5k <richardanatsui@gmail.com>
2021-01-15 05:21:48 +0000

    created events model

---
+++ models/Event.php
@@ -0,0 +1,29 @@
+<?php
+    class Event{
+        private $db;
+
+        public function __construct(){
+            $this->db = new Database;
+
+        }
+
+          
+
+        //gets the Event details
+        public function getEvents(){
+            //Prepare Query
+            $this->db->query('select * from donations');
+
+            //Execute
+            $this->db->execute();
+            
+
+            //Fetch One record
+            $results=$this->db->resultset();
+            return $results;
+            
+        }
+
+        
+    }
+?>
